1853854b93845b4814681f87d08af52a45848ac1
张召寒 <563638083@qq.com>
2023-10-20 10:43:30 +0800

    初始化basic application和entry point。

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//π©Hazel application  π”√
+
+#include<stdio.h>
+#include"Hazel/Application.h"
+
+// ---Entry Point------------------
+#include "Hazel/EntryPoint.h"
+// --------------------------------
