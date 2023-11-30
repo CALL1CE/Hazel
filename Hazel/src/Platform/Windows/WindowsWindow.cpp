9eaf6a3d4ec2bfbdcad4e22a9fc5f3606320e76e
CALL1CE <563638083@qq.com>
2023-11-30 16:47:17 +0800

    抽象了GraphicsContext，添加了OpenGL的context，并绘制了三角形

--- Hazel/src/Platform/Windows/WindowsWindow.cpp
+++ Hazel/src/Platform/Windows/WindowsWindow.cpp
@@ -4,7 +4,7 @@
 #include "Hazel/Events/MouseEvent.h"
 #include "Hazel/Events/KeyEvent.h"
 
-#include <glad/glad.h>
+#include "Platform/OpenGL/OpenGLContext.h"
 namespace Hazel {
 
 	static bool s_GLFWInitialized = false;
@@ -47,10 +47,14 @@ namespace Hazel {
 		}
 
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
-		glfwMakeContextCurrent(m_Window);
+		//glfwMakeContextCurrent(m_Window);
 
-		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-		HZ_CORE_ASSERT(status, "Failed to initialize Glad!");
+		//int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+		//HZ_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		m_Context = new OpenGLContext(m_Window);
+		m_Context->Init();
+
 
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
@@ -153,7 +157,8 @@ namespace Hazel {
 
 	void WindowsWindow::OnUpdate() {
 		glfwPollEvents();
-		glfwSwapBuffers(m_Window);
+		//glfwSwapBuffers(m_Window);
+		m_Context->SwapBuffers();
 	}
 
 	void WindowsWindow::SetVSync(bool enabled)
